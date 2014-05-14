39d623ed754df4092ceaaf79940d272b18810dda
Natapon Laohadet <ultragudget@gmail.com>
2014-05-14 12:22:16 +0700

    first commit

---
+++ mpdf/ttfontdata/thsarabunnew.mtx.php
@@ -0,0 +1,26 @@
+<?php
+$name='THSarabunNew';
+$type='TTF';
+$desc=array (
+  'Ascent' => 844,
+  'Descent' => -457,
+  'CapHeight' => 476,
+  'Flags' => 4,
+  'FontBBox' => '[-427 -421 947 836]',
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 692,
+);
+$up=-35;
+$ut=30;
+$ttffile='/Users/dekub/tmp/mpdf/ttfonts/THSarabunNew.ttf';
+$TTCfontID='0';
+$originalsize=479628;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='thsarabunnew';
+$panose=' 8 5 2 b 5 0 4 2 0 2 0 3';
+$haskerninfo=false;
+$unAGlyphs=false;
+?>
---
+++ mpdf/ttfontdata/thsarabunnewB.mtx.php
@@ -0,0 +1,26 @@
+<?php
+$name='THSarabunNew-Bold';
+$type='TTF';
+$desc=array (
+  'Ascent' => 844,
+  'Descent' => -457,
+  'CapHeight' => 476,
+  'Flags' => 262148,
+  'FontBBox' => '[-466 -457 947 844]',
+  'ItalicAngle' => 0,
+  'StemV' => 165,
+  'MissingWidth' => 698,
+);
+$up=-35;
+$ut=30;
+$ttffile='/Users/dekub/tmp/mpdf/ttfonts/THSarabunNewBold.ttf';
+$TTCfontID='0';
+$originalsize=366464;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='thsarabunnewB';
+$panose=' 8 5 2 b 5 0 4 2 0 2 0 3';
+$haskerninfo=false;
+$unAGlyphs=false;
+?>
